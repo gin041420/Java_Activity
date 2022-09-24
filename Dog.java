cc942f884821d468a0a83568037d3c07d8c31463
gin041420 <roginarolloque@gmail.com>
2022-09-24 21:40:23 +0800

    Added a file

---
+++ Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+    public void show(){
+        System.out.println("Dog inheriting from the Animal!");
+        System.out.println("Inside the method of the child class");
+        System.out.println("This is the dog class");
+    }
+}
